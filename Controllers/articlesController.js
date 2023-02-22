864023e8e7a8b943d50aca7ef747a22e6a95e68f
HaleemHussain <haleemhussain64@gmail.com>
2023-02-22 11:30:07 +0000

    completed ticket 5 with error handling

--- Controllers/articlesController.js
+++ Controllers/articlesController.js
@@ -1,7 +1,23 @@
-const {selectArticles} = require('../models/articleModel')
+const {selectArticles, selectArticleById} = require('../models/articleModel')
 
-exports.getAllArticles = (req, res) => {
-    selectArticles().then((result) => {
-        res.status(200).send({articles: result})
-    });
+exports.getAllArticles = (req, res, next) => {
+    selectArticles()
+        .then((result) => {
+            res.status(200).send({articles: result})
+        })
+        .catch((err) => {
+            next(err)
+        });
 }
+
+exports.getArticleById = (req, res, next) => {
+    const {article_id} = req.params;
+    selectArticleById(article_id)
+        .then((result) => {
+            res.status(200).send({article: result})
+        })
+        .catch((err) => {
+            next(err)
+        });
+}
+
--- Controllers/topicsController.js
+++ Controllers/topicsController.js
@@ -3,5 +3,7 @@ const {selectTopics} = require('../models/topicModel')
 exports.getAllTopics = (req, res) => {
     selectTopics().then((result) => {
         res.status(200).send({topics: result})
+    }).catch((err) => {
+        next(err)
     });
 }
