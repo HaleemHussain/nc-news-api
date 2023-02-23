d68bce35e14d242791d4af1d0d6a300c5639af0b
HaleemHussain <haleemhussain64@gmail.com>
2023-02-23 13:07:47 +0000

    made the refactoring chenges for the commentModel

--- Controllers/commentsController.js
+++ Controllers/commentsController.js
@@ -4,7 +4,6 @@ exports.getCommentsByArticleId = (req, res, next) => {
     const {article_id} = req.params;
     selectCommentsByArticleId(article_id)
         .then((result) => {
-            console.log(result)
             res.status(200).send({comments: result})
         })
         .catch((err) =>{
--- Models/articleModel.js
+++ Models/articleModel.js
@@ -9,7 +9,7 @@ exports.selectArticles = () => {
                        articles.created_at,
                        articles.votes,
                        articles.article_img_url,
-                       COUNT(comments.comment_id):: INT AS comment_count
+                       COUNT(comments.comment_id) :: INT AS comment_count
                 FROM articles
                          LEFT JOIN comments ON articles.article_id = comments.article_id
                 GROUP BY articles.article_id
