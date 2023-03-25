0b2fbfb5f4bf4453f04016f5d1b307968eb1529f
HaleemHussain <haleemhussain64@gmail.com>
2023-03-25 16:54:42 +0000

    added comment count

--- Models/articleModel.js
+++ Models/articleModel.js
@@ -49,23 +49,6 @@ exports.selectArticles = (order = "DESC", sort_by = "created_at", filter) => {
     });
 };
 
-// exports.selectArticleById = (article_id) => {
-//     return db
-//         .query(`SELECT *
-//                 FROM articles
-//                 WHERE article_id = $1;`, [article_id])
-//         .then(({rows}) => {
-//             const article = rows[0];
-//             if (!article) {
-//                 return Promise.reject({
-//                     status: 404,
-//                     msg: `No article found for article_id: ${article_id}`,
-//                 });
-//             }
-//             return article;
-//         });
-// };
-
 exports.selectArticleById = (article_id) => {
     const queryValues = [article_id];
 
