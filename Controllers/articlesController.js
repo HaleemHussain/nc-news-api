908b1610a3237d3e348819ce135f2d051e8a4d6d
HaleemHussain <haleemhussain64@gmail.com>
2023-03-25 15:58:49 +0000

    added queries for articles

--- Controllers/articlesController.js
+++ Controllers/articlesController.js
@@ -1,14 +1,22 @@
 const {selectArticles, selectArticleById, updateArticle} = require('../Models/articleModel')
 
 exports.getAllArticles = (req, res, next) => {
-    selectArticles()
-        .then((result) => {
-            res.status(200).send({articles: result})
+    const { sort_by, order } = req.query;
+    let filter = {};
+
+    for (let key in req.query) {
+        if (key !== "sort_by" && key !== "order") {
+            filter[key] = req.query[key];
+        }
+    }
+    selectArticles(order, sort_by, filter)
+        .then((articles) => {
+            res.status(200).send({ articles });
         })
         .catch((err) => {
-            next(err)
+            next(err);
         });
-}
+};
 
 exports.getArticleById = (req, res, next) => {
     const {article_id} = req.params;
--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -53,24 +53,103 @@ describe('app', () => {
     });
 
     describe('GET /api/articles', () => {
-        test("should respond with a json object of articles", () => {
+        test("200: responds with an array of article objects", () => {
             return request(app)
-                .get('/api/articles')
+                .get("/api/articles")
                 .expect(200)
                 .then(({body}) => {
                     const {articles} = body;
-                    expect(articles).toHaveLength(12)
+                    expect(articles).toHaveLength(12);
+                    expect(articles).toBeInstanceOf(Array);
                     articles.forEach((article) => {
-                        expect(article).toHaveProperty('author', expect.any(String))
-                        expect(article).toHaveProperty('title', expect.any(String))
-                        expect(article).toHaveProperty('article_id', expect.any(Number))
-                        expect(article).toHaveProperty('topic', expect.any(String))
-                        expect(article).toHaveProperty('created_at', expect.any(String))
-                        expect(article).toHaveProperty('votes', expect.any(Number))
-                        expect(article).toHaveProperty('article_img_url', expect.any(String))
-                        expect(article).toHaveProperty('comment_count', expect.any(Number))
+                        expect(article).toEqual(
+                            expect.objectContaining({
+                                article_id: expect.any(Number),
+                                title: expect.any(String),
+                                topic: expect.any(String),
+                                author: expect.any(String),
+                                body: expect.any(String),
+                                created_at: expect.any(String),
+                                votes: expect.any(Number),
+                                comment_count: expect.any(String),
+                            })
+                        );
                     });
                 });
+        });
+        describe("article queries", () => {
+            test("200: should respond with array sorted by article_id, order defaults to descending", () => {
+                return request(app)
+                    .get("/api/articles?sort_by=article_id")
+                    .expect(200)
+                    .then(({body}) => {
+                        const {articles} = body;
+                        expect(articles).toBeInstanceOf(Array);
+                        expect(articles).toBeSortedBy("article_id", {descending: true});
+                    });
+            });
+            test("200: should respond with array ordered by ascending, sort_by defaults to created_at", () => {
+                return request(app)
+                    .get("/api/articles?order=asc")
+                    .expect(200)
+                    .then(({body}) => {
+                        const {articles} = body;
+                        expect(articles).toBeInstanceOf(Array);
+                        expect(articles).toBeSortedBy("created_at", {ascending: true});
+                    });
+            });
+            test("200: should respond with array of one cat article object when filtered by topic cats", () => {
+                return request(app)
+                    .get("/api/articles?topic=cats")
+                    .expect(200)
+                    .then(({body}) => {
+                        const {articles} = body;
+                        expect(articles).toHaveLength(1);
+                        expect(articles[0]).toEqual(
+                            expect.objectContaining({
+                                article_id: 5,
+                                title: "UNCOVERED: catspiracy to bring down democracy",
+                                topic: "cats",
+                                author: "rogersop",
+                                body: "Bastet walks amongst us, and the cats are taking arms!",
+                                comment_count: "2",
+                                created_at: expect.any(String),
+                                votes: 0,
+                            })
+                        );
+                    });
+            });
+            test("200: for multiple queries, should respond with an array of mitch article objects, sorted by article_id with order ascending", () => {
+                return request(app)
+                    .get("/api/articles?topic=mitch&sort_by=article_id&order=asc")
+                    .expect(200)
+                    .then(({body}) => {
+                        const {articles} = body;
+                        expect(articles).toBeInstanceOf(Array);
+                        expect(articles).toBeSortedBy("article_id", {ascending: true});
+                        articles.forEach((article) => expect(article.topic).toBe("mitch"));
+                    });
+            });
+            test("400: stops invalid sort_by queries and responds with bad request", () => {
+                return request(app)
+                    .get("/api/articles?sort_by=quantity")
+                    .expect(400)
+                    .then(({body}) => {
+                        const {msg} = body;
+                        expect(msg).toBe("bad request: cannot sort by 'quantity'");
+                    });
+            });
+            test("400: stops invalid order queries and responds with bad request", () => {
+                return request(app)
+                    .get("/api/articles?order=rupaul")
+                    .expect(400)
+                    .then(({body}) => {
+                        const {msg} = body;
+                        expect(msg).toBe(
+                            "bad request: cannot order by 'rupaul', ASC or DESC only"
+                        );
+                    });
+            });
         });
     });
 
@@ -257,8 +336,8 @@ describe('app', () => {
                 .patch("/api/articles/1")
                 .send(updatedArticle)
                 .expect(200)
-                .then(({ body }) => {
-                    const { article } = body;
+                .then(({body}) => {
+                    const {article} = body;
                     expect(article).toEqual({
                         article_id: 1,
                         article_img_url: "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
@@ -279,8 +358,8 @@ describe('app', () => {
                 .patch("/api/articles/1")
                 .send(updatedArticle)
                 .expect(200)
-                .then(({ body }) => {
-                    const { article } = body;
+                .then(({body}) => {
+                    const {article} = body;
                     expect(article).toEqual({
                         article_id: 1,
                         article_img_url: "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
@@ -327,8 +406,8 @@ describe('app', () => {
                 .patch("/api/articles/1")
                 .send(updatedArticle)
                 .expect(400)
-                .then(({ body }) => {
-                    const { msg } = body;
+                .then(({body}) => {
+                    const {msg} = body;
                     expect(msg).toBe("bad request");
                 });
         });
@@ -338,8 +417,8 @@ describe('app', () => {
                 .patch("/api/articles/1")
                 .send(updatedArticle)
                 .expect(400)
-                .then(({ body }) => {
-                    const { msg } = body;
+                .then(({body}) => {
+                    const {msg} = body;
                     expect(msg).toBe("bad request");
                 });
         });
